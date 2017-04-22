e742c0c8067c5ff113d4f98ef6b3c8cb568dec88
tom <skatereal@hotmail.com>
2017-04-21 23:14:28 -0800

    Many changes to Client.py and Server.py At the moment, the two communicate with one another Setup state is complete, with image files holding state for inevitable video file integration.
    TODO: Need to integrate video files and the remaing states: Play, Pause, and TEARDOWN.

--- Server.py
+++ Server.py
@@ -8,14 +8,26 @@
 import sys
 import socket
 import random
+import pickle
+import sys
+import os
+import filecmp
 
 class Server:
     def __init__(self, listenPort = 33122, debug=False):
         self.port = listenPort
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind('', self.port)
         self.debug = debug
+        self.host = ""
+        self.sock.bind((self.host, self.port))
+        self.currentState = "setup"
         self.destAddress = ""
+        self.state = {
+            "setup": self.setup,
+            "play": self.play,
+            "pause": self.pause,
+            "teardown": self.teardown
+        }
 
     # receive function
     def receive(self):
@@ -23,9 +35,60 @@ class Server:
 
     def start(self):
         while True:
-            try:
-                message, self.destAddress = self.receive()
 
+            try:
+                print("Waiting for connection...")
+
+                pickledMsg, self.destAddress = self.receive()
+                message = pickle.loads(pickledMsg)
+
+                print("Connection received!")
+
+                while True:
+                    self.handleState(self.currentState)
 
             except (KeyboardInterrupt, SystemExit):
                 exit()
+
+    # state handler
+    def handleState(self, state):
+
+        if state == "setup":
+            setupDone = self.setup()
+            if setupDone:
+                self.currentState = "play"
+
+    # Handles the setup call from the client
+    def setup(self):
+        dirName = os.listdir(os.getcwd())
+        while True:
+            self.send(dirName, self.destAddress) # send list of files in directory to client TODO: Will have to add the encoding
+            pickledMsg, self.destAddress = self.receive() # receives filename and if encoding is ok for client
+            fileName = pickle.loads(pickledMsg) # checks to see if is in dir. if not, send contents of dir again
+            #if ok:
+            for file in dirName:
+                try:
+                    if filecmp.cmp(fileName, file):
+                        return True
+                except OSError:
+                    print('got an error')
+                    break
+            return False
+
+    # Todo: The following states
+    def play(self):
+        print("play")
+
+    def pause(self):
+        print("pause")
+
+    def teardown(self):
+        print("teardown")
+
+    def send(self, packet, address):
+        pickledMsg = pickle.dumps(packet)
+        self.sock.sendto(pickledMsg, address)
+
+if __name__ == "__main__":
+    s = Server()
+    s.start()
